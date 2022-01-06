557e45bc919538b408c5de772265da6cd5b1aeb4
Daniel <80741886+thangnguyen2359@users.noreply.github.com>
2022-01-07 04:58:43 +0700

    [Set up] add context

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,59 @@
+import { FC, ReactElement, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from '@mui/material'
+
+import { CryptoState, Currency } from "../../context/cryptoContext";
+
+const Header: FC = (): ReactElement => {
+  const navigate = useNavigate()
+  const { currency, setCurrency } = CryptoState()
+
+  return (
+    <AppBar position='static' color='transparent'>
+      <Container maxWidth='lg'>
+        <Toolbar>
+          <Typography
+            sx={{
+              flex: 1,
+              color: 'gold',
+              fontWeight: 'bold',
+              cursor: 'pointer',
+            }}
+            onClick={() => navigate('/')}
+          >
+            Crypto Tracker
+          </Typography>
+          <Select
+            variant='outlined'
+            labelId='crypto-type'
+            id='crypto-type'
+            value={currency}
+            label='Age'
+            onChange={e => {
+              // @ts-ignore
+              const value: Currency = e.target.value
+              setCurrency!(value)
+            }}
+            sx={{
+              width: 100,
+              height: 40,
+              marginLeft: 15,
+            }}
+          >
+            <MenuItem value='USD'>USD</MenuItem>
+            <MenuItem value='VND'>VND</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
+}
+
+export default Header
---
+++ src/context/cryptoContext.tsx
@@ -0,0 +1,33 @@
+import {
+  createContext,
+  Dispatch,
+  FC,
+  SetStateAction,
+  useContext,
+  useState,
+} from 'react'
+
+export type Currency = 'USD' | 'VND'
+
+interface CryptoContextInterface {
+  currency: Currency
+  setCurrency?: Dispatch<SetStateAction<Currency>>
+}
+
+const initialContextValue: CryptoContextInterface = {
+  currency: 'VND',
+}
+
+const CryptoCtx = createContext<CryptoContextInterface>(initialContextValue)
+
+export const CryptoContext: FC = ({ children }) => {
+  const [currency, setCurrency] = useState<Currency>('VND')
+
+  return (
+    <CryptoCtx.Provider value={{ currency, setCurrency }}>
+      {children}
+    </CryptoCtx.Provider>
+  )
+}
+
+export const CryptoState = () => useContext(CryptoCtx)
