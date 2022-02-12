61526849d648fd5f211f2cb905ab39474c93d553
Daniel <danielnguyen5201@gmail.com>
2022-02-13 04:30:38 +0700

    fix api link

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: 'https:api.coingecko.com/api/v3/coins',
+  baseURL: 'https://api.coingecko.com/api/v3/coins',
   timeout: 1000,
   headers: {
     "Content-type": "application/json"
